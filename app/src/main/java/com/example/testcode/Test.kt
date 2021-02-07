7ad69c0e4c2cdfaa1c8b9c28f0420280a6d81e3a
denden-kata <denden3104@gmail.com>
2021-02-07 21:48:09 +0900

    [update] : 掛け算追加

--- app/src/main/java/com/example/testcode/Test.kt
+++ app/src/main/java/com/example/testcode/Test.kt
@@ -1,7 +1,11 @@
 package com.example.testcode
 
 class Test {
-    fun sum (x: Int, y: Int) : Int {
+    fun sum(x: Int, y: Int): Int {
         return x + y
+    }
+
+    fun multiply(x: Int, y: Int): Int {
+        return x * y
     }
 }
